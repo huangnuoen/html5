261374b6246ca66c74fe8cc5738fa2ef9ca8b93f
huangnuoen <1261290560@qq.com>
2017-04-11 16:51:30 +0800

    add

--- position/a.js
+++ position/a.js
@@ -1,3 +1,7 @@
+var ourCoords = {
+	latitude: 47.624851,
+	longitude: -122.52099
+}
 window.onload = getMyLocation;
 function getMyLocation() {
 	if(navigator.geolocation) {
@@ -25,4 +29,26 @@ function displayError(error) {
 	}
 	var div = document.getElementById("location");
 	div.innerHTML = errorMessage;
+}
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	var Radius = 6371;
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads) * Math.cos(destLatRads) * Math.cos(startLongRads - destLongRads)) * Radius;
+	return distance;
+}
+function degreesToRadians(degrees) {
+	var radians = (degrees * Math.PI)/180;
+	return radians;
+}
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var div = document.getElementById("location");
+	div.innerHTML = "you are at latitude:" + latitude + ", longitude: " + longitude;
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "you are " + km + " km from the wickedlysmart hq";
 }
