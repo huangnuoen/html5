ff750d0829d8da0070d56483ccae8851099274cf
huangnuoen <1261290560@qq.com>
2017-04-19 17:27:22 +0800

    canvas

--- mighty/b.js
+++ mighty/b.js
@@ -1,4 +1,19 @@
+var lastReportTime = 0;
 window.onload = function() {
+	setInterval(handleRefresh, 10000);
+}
+function handleRefresh() {
+	var url = "http://gumball.wickedlysmart.com/?callback=updateSales" + "&lastreporttime=" + lastReportTime + "&random=" + (new Date()).getTime();
+	var newScriptElement = document.createElement("script");
+	newScriptElement.setAttribute("src", url);
+	newScriptElement.setAttribute("id", "jsonp");
+	var oldScriptElement = document.getElementById("jsonp");
+	var head = document.getElementsByTagName("head")[0];
+	if(oldScriptElement == null) {
+		head.appendChild(newScriptElement);
+	} else {
+		head.replaceChild(newScriptElement, oldScriptElement);
+	}
 }
 function updateSales(sales) {
 	var salesDiv = document.getElementById("sales");
@@ -7,5 +22,8 @@ function updateSales(sales) {
 		div.setAttribute("class", "saleItem");
 		div.innerHTML = sales[i].name + " sold " + sales[i].sales + " gumballs";
 		salesDiv.appendChild(div);
+	}
+	if(sales.length > 0) {
+		lastReportTime = sales[sales.length - 1].time;
 	}
 }
